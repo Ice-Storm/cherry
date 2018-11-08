8c0909e91f3b741a371bcc1ad71d19e250b4595b
ice <wcwz020140@163.com>
2018-11-08 20:30:57 +0800

    Update example

--- examples/main.go
+++ examples/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 
+	logging "cherrychain/clogging"
 	"cherrychain/p2p"
 
 	protocol "github.com/libp2p/go-libp2p-protocol"
@@ -14,15 +15,22 @@ import (
 
 var (
 	bootstrapPeers = []string{
+		"/ip4/0.0.0.0/tcp/9098/ipfs/QmZm5dYY6DSF5uXduZk99PtbmxRsNQ9vJad1RaR33rxhij",
+		// "/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+		// "/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",
+		// "/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+		// "/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+		// "/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
 		//"/ip4/0.0.0.0/tcp/4001/p2p/QmdSyhb8eR9dDSR5jjnRoTDBwpBCSAjT7WueKJ9cQArYoA",
 	}
+	log    = logging.MustGetLogger("MAIN")
+	isRoot = true
 )
 
 const (
 	ip         = "0.0.0.0"
 	protocolID = "/cherryCahin/1.0"
 	networkID  = "cherry-test"
-	isRoot     = false
 )
 
 func main() {
@@ -30,16 +38,16 @@ func main() {
 	dest := flag.String("d", "", "Dest MultiAddr String")
 	flag.Parse()
 
+	if *dest != "" {
+		isRoot = false
+		bootstrapPeers = append(bootstrapPeers, *dest)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	p2pNetwork := p2p.New(ctx, fmt.Sprintf("/ip4/%s/tcp/%d", ip, *port), isRoot)
-	log := p2pNetwork.Log.MustGetLogger("MAIN")
 
 	if err := p2pNetwork.StartSysEventLoop(ctx); err != nil {
 		cancel()
-	}
-
-	if *dest != "" {
-		bootstrapPeers = append(bootstrapPeers, *dest)
 	}
 
 	pID := protocol.ID(protocolID)
